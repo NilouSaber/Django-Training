677af86e4d12925742bb315b1286e20932f478e3
NilouSaber <nelssaber@gmail.com>
2024-08-30 20:14:57 +0330

    introducing several new modules

--- blog/models.py
+++ blog/models.py
@@ -4,3 +4,12 @@ from django.db import models
 class post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
+    # image =
+    # tags = 
+    # category = 
+    # author = 
+    countedViews = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    createdDate = models.DateTimeField(auto_now_add=True)
+    updatedTime = models.DateTimeField(auto_now=True)
+    publishedDate = models.DateTimeField(null=True)
--- website/models.py
+++ website/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class contact(models.Model):
+    name = models.CharField(max_length=255)
+    family = models.CharField(max_length=255)
+    email = models.EmailField()
+    subject = models.CharField(max_length=400)
+    message = models.TextField()
+    createdDate = models.DateTimeField(auto_now_add=True)
+    updateTime = models.DateTimeField(auto_now=True)
