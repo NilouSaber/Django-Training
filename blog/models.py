7f0be94fae6b8c282f8e6509f7011303e17dbbf7
NilouSaber <nelssaber@gmail.com>
2024-09-20 19:15:20 +0330

    rebuilding models

--- blog/models.py
+++ blog/models.py
@@ -13,8 +13,6 @@ class post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
     image = models.ImageField(upload_to='blog/', default='blog/default.jpg')
-    #thumbImage = models.ImageField(upload_to='blog_thumb/', default="blog_thumb/default.jpg")
-    #thumbImage = models.ImageField(upload_to='blog_thumb', height_field='60', width_field='60', max_length=100, default="blog_thumb/default.jpg")
     # tags = 
     category = models.ManyToManyField(Category)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
@@ -29,12 +27,6 @@ class post(models.Model):
     def __str__(self):
         return self.title
        
-def resize_image(self):
-        img = Image.open(self.image.path)
-        if img.height > 60 or img.width > 60:
-            output_size = (60, 60)
-            img.thumbnail(output_size)
-            img.save('blog_thumb')
 
     
     
