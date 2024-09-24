6daf406ac8599bbb2f37aa619fbed70ca753f6b0
NilouSaber <nelssaber@gmail.com>
2024-09-24 18:19:05 +0330

    Edits on the published date tags and contact db

--- website/models.py
+++ website/models.py
@@ -4,14 +4,14 @@ from django.db import models
 class contact(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField()
-    subject = models.CharField(max_length=400, blank=True)
+    subject = models.CharField(max_length=400, blank=True, null=True)
     message = models.TextField()
     createdDate = models.DateTimeField(auto_now_add=True)
     updateTime = models.DateTimeField(auto_now=True)
     class Meta:
         ordering = ("createdDate", )
     def __str__(self):
-        return self.subject
+        return self.subject if self.subject else "none"
 
     
 class newsletter(models.Model):
--- website/templatetags/website_tags.py
+++ website/templatetags/website_tags.py
@@ -1,9 +1,10 @@
 from django import template
 from blog.models import post, Category
-
+from django.utils.timezone import now
 register = template.Library()
 
+current_time = now()
 @register.inclusion_tag('website/recent.html')
 def recentposts(arg=6):
-    posts = post.objects.filter(status=1).order_by('-publishedDate')[:arg]
+    posts = post.objects.filter(publishedDate__lte=current_time,status=1).order_by('-publishedDate')[:arg]
     return {"posts":posts}
