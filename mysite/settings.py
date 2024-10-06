26457be729277f04b06d65b7c06b1c9c7bc1e8ac
NilouSaber <nelssaber@gmail.com>
2024-10-06 18:58:45 +0330

    password removed

--- mysite/settings.py
+++ mysite/settings.py
@@ -188,7 +188,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'mirastellings@gmail.com'
-EMAIL_HOST_PASSWORD = 'uaofjzniksgvqlre'
+EMAIL_HOST_PASSWORD = ''
 
 
 COMPRESS_ENABLED = True
